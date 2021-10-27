a1f1c68c5cce3c4339fb339951e3c4711b1db7b4
Abrar Ranjha <abrarranjha98@example.com>
2021-10-27 20:50:05 +0500

    from vs code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
-import Dashboard from "./Dashboard";
-import SignInOutContainer from "./components/container/index";
+// import Dashboard from "./Dashboard";
+// import SignInOutContainer from "./components/container/index";
 
-import { Redirect, Route, Switch } from "react-router-dom";
+// import { Redirect, Route, Switch } from "react-router-dom";
+import Table from "./components/pages/Table";
 const App = () => {
   return (
     <>
+      {/*
       <Switch>
         <Route exact path="/" component={SignInOutContainer} />
-        {/* <Route exact  path="/signup" component={Perfect} />
-<Route exact  path="/login" component={login} /> */}
-        <Route exact path="/dashboard" component={Dashboard} />
+           <Route exact  path="/signup" component={Perfect} />
+<Route exact  path="/login" component={login} /> 
+<Route exact path="/dashboard" component={Dashboard} />
 
-        <Redirect to="/" />
-      </Switch>
+<Redirect to="/" />
+</Switch>
+*/}
+      <Table />
     </>
   );
 };
